5990475feaeaff7482992f0ccafcb5d0e22b12fb
Marcelo-Diament <marcelodiament@gmail.com>
2021-04-24 23:46:43 -0300

    feature: exclude user route added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,8 +2,9 @@ const express = require('express'),
   router = express.Router(),
   controller = require('../controllers/users')
 
+router.post('/:id/delete', controller.delete)
+router.get('/:id', controller.index)
 router.get('/', controller.list)
 router.post('/', controller.add)
-router.get('/:id', controller.index)
 
 module.exports = router
